210e3debbecab28b510bef5e8915a3d334dd9114
João Correia <joaoafonsomarquescorriea@gmail.com>
2026-05-04 09:53:06 +0100

    first commit

---
+++ 14597_employes_managment/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using _14597_employes_managment.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _14597_employes_managment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ 14597_employes_managment/Models/UserActivity.cs
@@ -0,0 +1,11 @@
+﻿namespace _14597_employes_managment.Models
+{
+    public class UserActivity
+    {
+        public string?  CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public string? ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
